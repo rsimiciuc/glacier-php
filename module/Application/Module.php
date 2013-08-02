4e0a7a4596b5f6debcdd78c567851f5e08adcba3
Raul Simiciuc <byraul@gmail.com>
2013-08-03 00:40:37 +0100

    Initial commit

---
+++ module/Application/Module.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Application;
+
+use Zend\Mvc\ModuleRouteListener;
+use Zend\Mvc\MvcEvent;
+
+class Module
+{
+    public function onBootstrap(MvcEvent $e)
+    {
+        $eventManager        = $e->getApplication()->getEventManager();
+        $moduleRouteListener = new ModuleRouteListener();
+        $moduleRouteListener->attach($eventManager);
+    }
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function getConsoleUsage(\Zend\Console\Adapter\AbstractAdapter $console){
+        return array(
+            // Describe available commands
+            'user resetpassword [--verbose|-v] EMAIL'    => 'Reset password for a user',
+
+            // Describe expected parameters
+            array( 'EMAIL',            'Email of the user for a password reset' ),
+            array( '--verbose|-v',     '(optional) turn on verbose mode'        ),
+        );
+    }
+}
